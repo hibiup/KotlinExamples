9d65fe3221d0472b9e19f93c239b268440bf6eb0
Jeff <jeffwji@gmail.com>
2019-02-16 23:52:56 -0500

    Change class name

--- src/test/kotlin/example_1_hello/TestWhoIsOldest.kt
+++ src/test/kotlin/example_1_hello/TestWhoIsOldest.kt
@@ -3,7 +3,7 @@ package example_1_hello
 import kotlin.test.assertEquals
 import org.junit.Test as test
 
-class TestSource {
+class TestCases {
     @test
     fun whoIsOldestRetuensOldest() {
         assertEquals("Bob", findOldest(listOf(
